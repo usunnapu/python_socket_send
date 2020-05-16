cef7985c6dc92b54883e79263f382a729913b956
usunnapu <40170140+usunnapu@users.noreply.github.com>
2020-05-16 14:52:01 -0500

    Update send.py

--- send.py
+++ send.py
@@ -1,7 +1,7 @@
 import socket, sys
 conn = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-host = sys.argv[1]
-port = int(sys.argv[2])
+host = 'receive.app.svc.cluster.local'
+port = 4000
 conn.connect((host, port))
 print("Connected to host " + sys.argv[1])
 td = 1
